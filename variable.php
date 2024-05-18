55c0462d06f9828fd619ff3303e9cb4b888c7665
Aman Bisht <bishtaman60@gmail.com>
2024-05-18 19:12:26 +0530

    variable

--- variable.php
+++ variable.php
@@ -13,3 +13,14 @@ echo "</br>";
 echo $x;
 
 ?>
+
+// var are smart they can understand why type of data type are those 
+// we dont have to tell or desc the type 
+
+//    variable rule 
+// var start with $ sign
+// var name cannot start with num & special char
+// var name must start with a letter or the underscore (_) char
+// var name can only contain alpha-numeric char and underscore
+// $name , $Name and $NAME are threee diff variable 
+// syntax -> $x = "AMAN";
